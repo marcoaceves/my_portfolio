6506b9fceb9e6f7b8704b73bdb3a13434fefb8ec
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2023-01-28 19:11:43 -0800

    individaul brwaler data set up

--- flask_app/controllers/brawl.py
+++ flask_app/controllers/brawl.py
@@ -7,21 +7,27 @@ import flask_app.controllers.creds as creds
 token=creds.brawl_token
 @app.route('/brawlstars')
 def brawlers():
-    # random date generator
-    tokens = token.strip()
-    headers = {"Authorization": f"Bearer {tokens}"}
-
-    
-
     url= f'https://api.brawlapi.com/v1/brawlers'
 
     response = requests.get(url) 
 
-
-    
     brawlers_data = response.json()
     # print(brawlers_data, "random data")
-    print(brawlers_data["list"][1]['id'], "random data")
+    print(brawlers_data["list"][1], "random data")
 
 
     return render_template('brawl.html', brawlers_data=brawlers_data)
+
+
+@app.route('/brawler/<int:brawler_id>')
+def display_brawler(brawler_id):
+    url= f'https://api.brawlapi.com/v1/brawlers/{brawler_id}'
+
+    response = requests.get(url) 
+
+    brawlers_data = response.json()
+    # print(brawlers_data, "random data")
+    print(brawlers_data, "brawler_data")
+
+
+    return render_template('brawler.html', brawlers_data=brawlers_data )
